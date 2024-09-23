8b4f54f5b166faa6ea2772f4fe05144dceeb6b8b
nina <nina.savoy@icloud.com>
2024-09-22 22:12:54 -0300

    primeiro

---
+++ src/main/java/insper/interzambom/InterzambomApplication.java
@@ -0,0 +1,13 @@
+package insper.interzambom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterzambomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterzambomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/insper/interzambom/InterzambomApplicationTests.java
@@ -0,0 +1,13 @@
+package insper.interzambom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterzambomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
